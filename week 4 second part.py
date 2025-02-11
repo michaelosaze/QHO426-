8eb39cef151891d5275814fbd2978968f61560bc
Osaze Aibhuedefe <0aibho14@solent.ac.uk>
2025-02-10 16:37:14 -1100

    version 2.0 (using functions)

--- week 4 second part.py
+++ week 4 second part.py
@@ -1,18 +1,12 @@
-valid_input = False
-while not valid_input:
-    height = input("Enter yout height in cm: ")
-    if height.isnumeric():
-        valid_input = True
-    else:
-        print("You must enter a number")
+def check_numeric(prompt):
+    valid_input =False
+    while not valid_input:
+        user_input = input(f"Enter your {prompt}:")
+        if user_input.isnumeric():
+            valid_input = True
+        else:
+            print("You must enter a number")
+    return int(user_input)
 
-valid_input = False
-while not valid_input:
-    weight = input("Enter your weight in Kg: ")
-    if weight. isnumeric():
-        valid_input =True
-    else:
-        print("You must enter a number")
-
-bmi = int(weight)/(int(height)/100)**2
-print(f"Your BMI is {bmi}")
+bmi = check_numeric("weight in kg")/(check_numeric("height in cm")/100)**2
+print(f"your BMI IS {bmi}")
